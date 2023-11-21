2f4e1e1dbf340ef8b815777d56d9bf439c7e1a48
Sakshi <sakshijain1148@gmail.com>
2023-11-21 11:52:36 +0530

    Initial Commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: <YOURAPIKEY>,
-  authDomain: <YOURAUTHDOAMIN>,
-  projectId: <YOURPROJECTID>,
-  storageBucket: <YOURSTORAGEBUCKET>,
-  messagingSenderId: <YOURMESSAGINGSENDERID>,
-  appId: <YOURAPPID>,
+  apiKey: "AIzaSyA6OjFmK2vMwsDoq6ppcU77l_h3BtJIXbc",
+  authDomain: "react-authentication-68a40.firebaseapp.com",
+  projectId: "react-authentication-68a40",
+  storageBucket: "react-authentication-68a40.appspot.com",
+  messagingSenderId: "532092892389",
+  appId: "1:532092892389:web:57801708df70d5bd0d500c",
 };
 
 // Initialize Firebase
